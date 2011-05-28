64f6282ef1b5ceb42651731cf4ce4acb29fbfd3f
Jeremy Kahn <jeremyckahn@gmail.com>
2011-05-28 11:59:21 -0500

    Added some test stuff.

--- php/test.php
+++ php/test.php
@@ -1,12 +1,10 @@
 <?php
 
-echo("running... \r\n");
+//echo("running... \r\n");
 
 
 $ch = curl_init("http://api.dostuffmedia.com/bands.json?key=af9bf4e58022305398c885ae8a2e9888725dab0a");
-$fp = fopen("test.txt", "w");
 
-curl_setopt($ch, CURLOPT_FILE, $fp);
 curl_setopt($ch, CURLOPT_HEADER, 0);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
@@ -14,9 +12,19 @@ $response = curl_exec($ch);
 
 $responseArr = json_decode($response, true);
 
-print_r($responseArr);
+
+
+$fp = fopen('output.json', 'w');
+fwrite($fp, $response);
+fclose($fp);
+
+
+
+
+$response = json_encode($responseArr["bands"]);
+
+echo($response);
 
 curl_close($ch);
-fclose($fp);
 
 ?>
